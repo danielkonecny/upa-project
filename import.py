d7221e3ec3cc967d02bf7835ab3595e2b3ad3903
Daniel Konečný <dkonecny97@gmail.com>
2020-10-21 14:10:51 +0200

    New import script

---
+++ import.py
@@ -0,0 +1,38 @@
+from pymongo import MongoClient
+import datetime
+
+client = MongoClient('localhost', 27017)
+
+db = client['test-database']
+collection = db['test-collection']
+
+print(client.list_database_names())
+
+"""
+post = {"author": "Mike",
+        "text": "My first blog post!",
+        "tags": ["mongodb", "python", "pymongo"],
+        "date": datetime.datetime.utcnow()}
+
+post_id = collection.insert_one(post).inserted_id
+"""
+
+mylist = [
+  { "name": "Amy", "address": "Apple st 652"},
+  { "name": "Hannah", "address": "Mountain 21"},
+  { "name": "Michael", "address": "Valley 345"},
+  { "name": "Sandy", "address": "Ocean blvd 2"},
+  { "name": "Betty", "address": "Green Grass 1"},
+  { "name": "Richard", "address": "Sky st 331"},
+  { "name": "Susan", "address": "One way 98"},
+  { "name": "Vicky", "address": "Yellow Garden 2"},
+  { "name": "Ben", "address": "Park Lane 38"},
+  { "name": "William", "address": "Central st 954"},
+  { "name": "Chuck", "address": "Main Road 989"},
+  { "name": "Viola", "address": "Sideway 1633"}
+]
+
+x = collection.insert_many(mylist)
+
+for x in collection.find({}, {"_id": 0, "name": 1, "address": 1}):
+    print(x)
