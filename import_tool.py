7011becc7a82ff200aa29a47c56a2b8ef5d89342
Tomáš Ryšavý <xrysav27@vutbr.cz>
2020-11-21 00:00:22 +0100

    importing relations between dates in district

--- import_tool.py
+++ import_tool.py
@@ -1,4 +1,5 @@
 import json
+import datetime
 from neo4j import GraphDatabase
 
 with open('CiselnikOkresu.json', encoding="utf8") as json_file:
@@ -21,12 +22,23 @@ def generate_districts(tx, iname, icode, idate, iincInf, iincDead, iincCured):
            name=iname, code=icode, date=idate, incInf=iincInf, incDead=iincDead, incCured=iincCured)
 
 
-def generate_relations(tx, dist_1_code, dist_2_code):
+def generate_relations_between_districts(tx, dist_1_code, dist_2_code):
     tx.run(
         "MATCH (a:District), (b:District) "
         "WHERE a.code = $dist1 AND b.code = $dist2 AND a.date = b.date "
         "CREATE (a)-[:IS_NEIGHBOUR]->(b)",
         dist1=dist_1_code, dist2=dist_2_code)
+
+
+def generate_relations_between_dates_in_district(tx, dist_1_code, date1, date2):
+    print("MATCH (a:District), (b:District) "
+        "WHERE a.code = \'", dist_1_code, "\' AND a.date = \'", date1, "\' AND b.code = \'", dist_1_code, "\' AND b.date = \'", date2, "\' "
+        "CREATE (a)-[:NEXT_DAY]->(b)")
+    tx.run(
+        "MATCH (a:District), (b:District) "
+        "WHERE a.code = $dist AND a.date = $date_1 AND b.code = $dist AND b.date = $date_2 "
+        "CREATE (a)-[:NEXT_DAY]->(b)",
+        dist=dist_1_code, date_1=date1, date_2=date2)
 
 
 with driver.session() as session:
@@ -36,7 +48,6 @@ with driver.session() as session:
         for district in Districts:
             if district['code'] == code:
                 name = district['name']
-                # print(name)
         date = data['datum']
         incInf = data['kumulativni_pocet_nakazenych']
         incDead = data['kumulativni_pocet_vylecenych']
@@ -46,19 +57,25 @@ with driver.session() as session:
     for relation in DistrictsRelations:
         dist1 = relation['dist1']
         dist2 = relation['dist2']
-        session.write_transaction(generate_relations, dist1, dist2)
+        session.write_transaction(generate_relations_between_districts, dist1, dist2)
 
 
-def create_friend_of(tx, name, friend):
-    tx.run("CREATE (a:Person)-[:KNOWS]->(f:Person {name: $friend}) "
-           "WHERE a.name = $name "
-           "RETURN f.name AS friend", name=name, friend=friend)
+def get_all_dates(tx):
+    result = tx.run("MATCH (n:District) RETURN DISTINCT n.date")
+    dates = []
+    for ix, record in enumerate(result):
+        dates.append(record.values())
+    return dates
 
 
-# with driver.session() as session:
-#     session.write_transaction(create_friend_of, "Alice", "Bob")
-
-# with driver.session() as session:
-#     session.write_transaction(create_friend_of, "Alice", "Carl")
+with driver.session() as session:
+    all_dates = session.read_transaction(get_all_dates)
+    all_dates = sorted(all_dates, key=lambda x: datetime.datetime.strptime(x[0], '%Y-%m-%d'))
+    for district in Districts:
+        previous_date = -1
+        for date in all_dates:
+            if previous_date != -1:
+                session.write_transaction(generate_relations_between_dates_in_district, district['code'], previous_date[0], date[0])
+            previous_date = date
 
 driver.close()
