f9eea4ca5b914d964fe4cfa75fa83778e9ad2486
Filip Jerabek <jerabek.rou@gmail.com>
2020-11-22 20:50:20 +0100

    update tool working (using smaller datasets]

--- update_tool.py
+++ update_tool.py
@@ -12,12 +12,12 @@ def generate_districts(tx, iname, icode, idate, iincInfec, iincDead, iincCured):
            name=iname, code=icode, date=idate, incInfec=iincInfec, incDead=iincDead, incCured=iincCured)
 
 
-def generate_relations_between_districts(tx, dist_1_code, dist_2_code):
+def generate_relations_between_districts(tx, dist_1_code, dist_2_code, update_date):
     tx.run(
         "MATCH (a:District), (b:District) "
-        "WHERE a.code = $dist1 AND b.code = $dist2 AND a.date = b.date "
+        "WHERE a.code = $dist1 AND b.code = $dist2 AND a.date = b.date AND a.date >= $date_from "
         "CREATE (a)-[:IS_NEIGHBOR]->(b)",
-        dist1=dist_1_code, dist2=dist_2_code)
+        dist1=dist_1_code, dist2=dist_2_code, date_from=update_date)
 
 
 def generate_relations_between_dates_in_district(tx, dist_1_code, date1, date2):
@@ -28,12 +28,24 @@ def generate_relations_between_dates_in_district(tx, dist_1_code, date1, date2):
         dist=dist_1_code, date_1=date1, date_2=date2)
 
 
-def get_all_dates(tx):
-    result = tx.run("MATCH (n:District) RETURN DISTINCT n.date")
+def get_dates_after(tx, idate):
+    result = tx.run(
+        "MATCH (n:District) "
+        "WHERE n.date >= $date "
+        "RETURN DISTINCT n.date",
+        date=idate
+    )
     dates = []
     for ix, record in enumerate(result):
         dates.append(record.values())
     return dates
+
+def get_last_date(tx):
+    result = tx.run("MATCH (n:District) RETURN max(n.date)")
+    dates = []
+    for ix, record in enumerate(result):
+        dates.append(record.values())
+    return dates[0][0]
 
 
 with open('districts_names_codes.json', encoding="utf8") as json_file:
@@ -42,12 +54,16 @@ with open('districts_names_codes.json', encoding="utf8") as json_file:
 with open('districts_neighbors_relations.json', encoding="utf8") as json_file:
     DistrictsRelations = json.load(json_file)
 
-with open('data_testing_2.json', encoding="utf8") as json_file:
+with open('data_update_2.json', encoding="utf8") as json_file:
     CoronaData = json.load(json_file)
-    # print(CoronaData)
 
 driver = GraphDatabase.driver(uri, auth=("neo4j", PASS_TO_DATABASE))
 
+lastestDateInDb = "0"
+with driver.session() as session:
+    lastestDateInDb = session.read_transaction(get_last_date)
+
+earliestDate = "2222-10-6"
 with driver.session() as session:
     for data in CoronaData['data']:
         code = data['okres_lau_kod']
@@ -56,22 +72,28 @@ with driver.session() as session:
             if district['code'] == code:
                 name = district['name']
         date = data['datum']
+        if date < earliestDate:
+            earliestDate = date
         incInf = data['kumulativni_pocet_nakazenych']
         incDead = data['kumulativni_pocet_vylecenych']
         incCured = data['kumulativni_pocet_vylecenych']
         session.write_transaction(generate_districts, name, code, date, incInf, incDead, incCured)
 
+    # subtract one day from date
+    # datetime_object = datetime.datetime.strptime(earliestDate, '%Y-%m-%d') - datetime.timedelta(days=1)
+    # earliestDate = datetime_object.strftime('%Y-%m-%d')
+
     for relation in DistrictsRelations:
         dist1 = relation['dist1']
         dist2 = relation['dist2']
-        session.write_transaction(generate_relations_between_districts, dist1, dist2)
+        session.write_transaction(generate_relations_between_districts, dist1, dist2, earliestDate)
 
 with driver.session() as session:
-    all_dates = session.read_transaction(get_all_dates)
-    all_dates = sorted(all_dates, key=lambda x: datetime.datetime.strptime(x[0], '%Y-%m-%d'))
+    dates = session.read_transaction(get_dates_after, lastestDateInDb)
+    dates = sorted(dates, key=lambda x: datetime.datetime.strptime(x[0], '%Y-%m-%d'))
     for district in Districts:
         previous_date = -1
-        for date in all_dates:
+        for date in dates:
             if previous_date != -1:
                 session.write_transaction(generate_relations_between_dates_in_district, district['code'],
                                           previous_date[0], date[0])
