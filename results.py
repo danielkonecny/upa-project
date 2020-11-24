87135f5e02b65d3b35ab668573cfd7cc3067ceea
Daniel Konečný <dkonecny97@gmail.com>
2020-11-25 00:12:11 +0100

    Clean code for generating plots

--- results.py
+++ results.py
@@ -54,8 +54,9 @@ def select_inc_infec_mov_avg(s_conn, s_period=7, s_from_date='0000-00-00', s_to_
     return cur.fetchall()
 
 
-def plot_incerase_percentage():
+def plot_increase_percentage(from_date, to_date, period, district='all'):
     global dates_abs, data_abs, dates_per, data_per, dates_mov_avg, data_mov_avg
+    assert period == 3 or period == 7 or period == 14 or period == 28
 
     plt.rcParams.update({'font.size': 6})
 
@@ -64,10 +65,10 @@ def plot_incerase_percentage():
     # change of infected
     change_line = ax1.bar(dates_abs, data_abs, color="grey", width=0.65)
     # moving average
-    average_line, = ax1.plot(dates_mov_avg, data_mov_avg, color="#A346A3", LineWidth=0.6)
+    average_line, = ax1.plot(dates_mov_avg, data_mov_avg, color="#A346A3", linewidth=0.6)
 
-    # zero line for change of infecter
-    ax1.axhline(y=0, color='black', LineWidth=0.3)
+    # zero line for change of infected
+    ax1.axhline(y=0, color='black', linewidth=0.3)
 
     step = 1
     if len(dates_abs) > 13:
@@ -77,87 +78,62 @@ def plot_incerase_percentage():
     plt.setp(ax1.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
 
     # zero line for percentage
-    ax2.axhline(y=0, color='#FFA5C5', LineWidth=0.3)
+    ax2.axhline(y=0, color='#FFA5C5', linewidth=0.3)
 
     # percentage change
-    percentage_line, = ax2.plot(dates_per, data_per, color="red", LineWidth=0.5)
+    percentage_line, = ax2.plot(dates_per, data_per, color="red", linewidth=0.5)
     ax2.set_ymargin(0.25)
 
     ax1.set_ylabel("Change in the number of infected ")
     ax2.set_ylabel("Percentage change of infected")
     ax1.set_xlabel("Date")
-    ax1.legend([change_line, average_line], [f"""{period}-day step moving average""", 'change of infected'], loc='upper left')
+    ax1.legend([change_line, average_line],
+               [f"{period}-day step moving average", 'change of infected'], loc='upper left')
     ax2.legend([percentage_line], ['percentage infected change'], loc='upper right')
 
     fig.tight_layout()
     if district == 'all':
-        plt.title("Increase/decrease of infected in Czech")
+        plt.title("Increase/decrease of infected in Czechia")
     else:
-        plt.title(f"""Increase/decrease of infected in {district}""")
+        plt.title(f"Increase/decrease of infected in {district}")
 
-    plt.savefig(f"""images/{district}_{from_date}_{to_date}_{period}.svg""", bbox_inches='tight')
+    plt.savefig(f"images/{district}_{from_date}_{to_date}_{period}.svg", bbox_inches='tight')
     plt.show()
 
 
-def calculate_data_for_graph():
+def get_formatted_float_data(unformatted_data, data_type):
+    unformatted_data = np.swapaxes(np.array(unformatted_data), 0, 1)
+    dates = unformatted_data[0]
+    data = list(map(data_type, unformatted_data[1]))
+    return data, dates
+
+
+def calculate_data_for_graph(from_date, to_date, period, district='all'):
     global dates_abs, data_abs, dates_per, data_per, dates_mov_avg, data_mov_avg
+    assert period == 3 or period == 7 or period == 14 or period == 28
 
     inc_infec_abs = select_inc_infec_abs(conn, from_date, to_date, district)
-    print(inc_infec_abs)
-    inc_infec_abs = np.swapaxes(np.array(inc_infec_abs), 0, 1)
-    dates_abs = inc_infec_abs[0]
-    data_abs = list(map(int, inc_infec_abs[1]))
+    data_abs, dates_abs = get_formatted_float_data(inc_infec_abs, data_type=int)
 
     inc_infec_per = select_inc_infec_per(conn, from_date, to_date, district)
-    print(inc_infec_per)
-    inc_infec_per = np.swapaxes(np.array(inc_infec_per), 0, 1)
-    dates_per = inc_infec_per[0]
-    # data_per = list(map(float, inc_infec_per[1]))
-
-    data_per = []
-    for item in inc_infec_per[1]:
-        if item is not None:
-            data_per.append(float(item))
-        else:
-            data_per.append(0.0)
-    print(data_per)
+    data_per, dates_per = get_formatted_float_data(inc_infec_per, data_type=float)
 
     inc_infec_mov_avg = select_inc_infec_mov_avg(conn, period, from_date, to_date, district)
-    inc_infec_mov_avg = np.swapaxes(np.array(inc_infec_mov_avg), 0, 1)
-    dates_mov_avg = inc_infec_mov_avg[0]
-    # data_mov_avg = list(map(float, inc_infec_mov_avg[1]))
-    data_mov_avg = []
-    for item in inc_infec_mov_avg[1]:
-        if item is not None:
-            data_mov_avg.append(float(item))
-        else:
-            data_mov_avg.append(0.0)
+    data_mov_avg, dates_mov_avg = get_formatted_float_data(inc_infec_mov_avg, data_type=float)
 
 
 with conn:
     global dates_abs, data_abs, dates_per, data_per, dates_mov_avg, data_mov_avg
-    from_date = '2019-03-01'
-    to_date = '2021-04-11'
-    period = 14  # periods = [3, 7, 14, 28]
-    district = "all"
+    # periods = [3, 7, 14, 28]
 
-    calculate_data_for_graph()
-    plot_incerase_percentage()
+    calculate_data_for_graph(from_date='2019-03-01', to_date='2021-04-11', period=14)
+    plot_increase_percentage(from_date='2019-03-01', to_date='2021-04-11', period=14)
 
-    from_date = '2020-04-01'
-    to_date = '2020-08-30'
-    period = 7
-    district = "Brno-město"
+    calculate_data_for_graph(from_date='2020-04-01', to_date='2020-08-30', period=7, district="Brno-město")
+    plot_increase_percentage(from_date='2020-04-01', to_date='2020-08-30', period=7, district="Brno-město")
 
-    calculate_data_for_graph()
-    plot_incerase_percentage()
+    calculate_data_for_graph(from_date='2020-04-01', to_date='2020-08-30', period=7, district="Znojmo")
+    plot_increase_percentage(from_date='2020-04-01', to_date='2020-08-30', period=7, district="Znojmo")
 
-    district = "Znojmo"
-
-    calculate_data_for_graph()
-    plot_incerase_percentage()
-
-    district = "Brno-venkov"
-
-    calculate_data_for_graph()
-    plot_incerase_percentage()
+    calculate_data_for_graph(from_date='2020-04-01', to_date='2020-08-30', period=7, district="Brno-venkov")
+    plot_increase_percentage(from_date='2020-04-01', to_date='2020-08-30', period=7, district="Brno-venkov")
