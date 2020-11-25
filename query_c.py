e99773d9fda6ad14abdf3c831bbfcf90d7012593
Daniel Konečný <dkonecny97@gmail.com>
2020-11-25 09:33:45 +0100

    New file for displaying results

---
+++ query_c.py
@@ -0,0 +1,21 @@
+import sqlite3
+from sqlite3 import Error
+import matplotlib.pyplot as plt
+import numpy as np
+
+database = r"relation.db"
+
+
+def create_connection(db_file=':memory:'):
+    connect_conn = None
+    try:
+        connect_conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+    return connect_conn
+
+
+conn = create_connection(database)
+
+with conn:
+    print("Connected")
